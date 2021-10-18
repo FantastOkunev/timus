3968c5ecdba4ff33804f2ee40d27c60346b7f35d
FantastOkunev <okunev229@yandex.ru>
2021-10-18 22:56:39 +0300

    1296

--- timus.cpp
+++ timus.cpp
@@ -6,23 +6,43 @@ using namespace std;
 
 int main()
 {
-    int64_t i, j, k, s;
+    int64_t i, j;
 
     int64_t n;
     cin >> n;
 
-    int64_t elem;
+    int64_t elem, size = 1;
     vector<int64_t> matrix;
     matrix.reserve(n + 1);
     matrix.push_back(0);
-    for (i = 0; i < n; j++)
+    for (i = 0; i < n; i++)
     {
         cin >> elem;
-        matrix.push_back(elem);
+        if (elem == 0)
+        {
+            continue;
+        }
+        if (elem * matrix[size - 1] <= 0)
+        {
+            matrix.push_back(elem);
+            size++;
+        }
+        else
+        {
+            matrix[size - 1] += elem;
+        }
     }
 
+    // cout << endl;
+    // for (i = 0; i < size; i++)
+    // {
+    //     cout << matrix[i] << " ";
+    // }
+
+    // cout << endl;
+
     int64_t max = 0, cur_sum;
-    for (i = 1; i < n + 1; i++)
+    for (i = 1; i < size; i++)
     {
         matrix[i] = matrix[i] + matrix[i - 1];
         for (j = 1; j < i + 1; j++)
