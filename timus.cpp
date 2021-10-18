c0e8e7857cbcbac64983bec072adc1a88adabbb7
FantastOkunev <okunev229@yandex.ru>
2021-10-18 22:19:37 +0300

    1296 1

--- timus.cpp
+++ timus.cpp
@@ -12,54 +12,28 @@ int main()
     cin >> n;
 
     int64_t elem;
-    vector<vector<int64_t>> matrix;
-    matrix.resize(n + 1);
-    for (i = 0; i < n; i++)
+    vector<int64_t> matrix;
+    matrix.reserve(n + 1);
+    matrix.push_back(0);
+    for (i = 0; i < n; j++)
     {
-        matrix[i].reserve(n + 1);
-        matrix[i].push_back(0);
-        for (j = 0; j < n; j++)
-        {
-            cin >> elem;
-            matrix[i].push_back(elem);
-        }
-    }
-    for (j = 0; j < n; j++)
-    {
-        matrix[i].resize(n + 1);
+        cin >> elem;
+        matrix.push_back(elem);
     }
 
-    int64_t max = matrix[n - 1][1], cur_sum;
-    for (i = n - 1; i >= 0; i--)
+    int64_t max = 0, cur_sum;
+    for (i = 1; i < n + 1; i++)
     {
-        for (j = 1; j < n + 1; j++)
+        matrix[i] = matrix[i] + matrix[i - 1];
+        for (j = 1; j < i + 1; j++)
         {
-            matrix[i][j] = matrix[i][j] + matrix[i + 1][j] + matrix[i][j - 1] - matrix[i + 1][j - 1];
-            for (k = n - 1; k >= i; k--)
+            cur_sum = matrix[i] - matrix[j - 1];
+            if (cur_sum > max)
             {
-                for (s = 1; s < j + 1; s++)
-                {
-                    cur_sum = matrix[i][j] - matrix[i][s - 1] - matrix[k + 1][j] + matrix[k + 1][s - 1];
-                    if (cur_sum > max)
-                    {
-                        max = cur_sum;
-                    }
-                }
+                max = cur_sum;
             }
         }
     }
-
-    // cout << endl;
-
-    // for (i = 0; i < n + 1; i++)
-    // {
-    //     for (j = 0; j < n + 1; j++)
-    //     {
-    //         cout << matrix[i][j] << " ";
-    //     }
-    //     cout << endl;
-    // }
-    // cout << endl;
 
     cout << max;
 
